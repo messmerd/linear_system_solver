b8e0e7b66f4386a75dd2075dd7d2f819712567eb
messmerd <messmerd97@gmail.com>
2018-05-26 13:29:43 -0400

    Tidied up the code in main.cpp

--- EquationSolver/main.cpp
+++ EquationSolver/main.cpp
@@ -6,31 +6,29 @@ int main(int argc, char *argv[])
 	std::cout.precision(10);
 	std::cout.showpoint;
 
-	if (argc > 1)
+	LinearSystem *equations;
+
+	if (argc > 1)  // If a command-line argument was given
 	{
-		LinearSystem *equations = new LinearSystem(argv[1]);
-		equations->displayEquations();
-		equations->displayMatrix(); 
-		equations->Solve();
-		equations->displayResults(); 
-		equations->displayMatrix();
-		std::cout << std::endl; 
-		delete equations; 
+		equations = new LinearSystem(argv[1]);  // Use the command-line argument as the equations 
 	}
 	else
 	{
 		//LinearSystem equations1("-A +2B    +3C = 4 ; 5A + 6.68B-7C=10;-11A+0.5B-3.79C=-0.234;");
-		LinearSystem *equations1 = new LinearSystem("3x+2y-3z=-13 ; -3y+4x+6z=7; -z+x=-5    ");
-		std::cout << "The system of equations to solve is:\n\n";
-		equations1->displayEquations(); 
-		equations1->Solve();
-		//equations1.displayEquations();
-		//equations1.displayMatrix(); 
-		std::cout << "\nThe solutions are:\n\n";
-		equations1->displayResults(); 
-		std::cout << std::endl;
-		delete equations1; 
+		equations = new LinearSystem("3x+2y-3z=-13 ; -3y+4x+6z=7; -z+x=-5    ");  // Use a default test case 
 	}
+
+	std::cout << "The equations:\n\n";
+	equations->displayEquations();
+	std::cout << "\nThe augmented matrix:\n\n";
+	equations->displayMatrix(); 
+	equations->Solve();
+	std::cout << "\nThe augmented matrix after reduction: \n\n"; 
+	equations->displayMatrix();
+	std::cout << "\nThe results:\n\n"; 
+	equations->displayResults(); 
+	std::cout << std::endl; 
+	delete equations; 
 
 	return 0;
 }
